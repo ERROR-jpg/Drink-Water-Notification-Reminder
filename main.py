64ae15e67e7868b30c94af32d98fef88f98561ab
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-02-01 18:26:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         notification.notify(
             title = "Please Drink Water!!",
             message = '''Health experts commonly recommend eight 8-ounce glasses, which equals about 2 liters, or half a gallon a day. This is called the 8x8 rule and is very easy to remember.''',
-            app_icon = ("C:\\Users\\jaymi\\Desktop\\Python Projects\\Water notification system\\icon.ico"),   
+            app_icon = ("C:\\Users\\jaymin\\Desktop\\Python Projects\\Water notification system\\icon.ico"),   
             timeout = 10
         )
         time.sleep(60*60)
